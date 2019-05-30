01907459685a6739762fe7635b392b9f56bf0299
S-Marks <48846653+S-Marks@users.noreply.github.com>
2019-05-30 12:06:12 -0700

    Update find-waldo.js

--- find-waldo.js
+++ find-waldo.js
@@ -1,11 +1,4 @@
-// The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-/*  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }*/
-
   var names = arr;
 
   names.forEach(found);
@@ -17,13 +10,3 @@ function actionWhenFound(ele, ind) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-/*
-Modify callback function.
-Get index of Waldo.
-
-Output: "Found Waldo at index 2."
-
-instead of for loop, use forEach
-
-*/
