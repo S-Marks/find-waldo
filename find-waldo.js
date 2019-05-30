570550a79bf94bb60e025d249688127b0d511310
Bobby <bobbyhplau@gmail.com>
2019-05-30 18:58:15 +0000

    Callback function forEach loop

--- find-waldo.js
+++ find-waldo.js
@@ -1,14 +1,19 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+/*  for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
     }
-  }
+  }*/
+
+  var names = arr;
+
+  names.forEach(found);
 }
 
-function actionWhenFound(index) {
-  console.log("Found Waldo at index " + index + ".");
+function actionWhenFound(ele, ind) {
+  if (ele === "Waldo")
+  console.log("Found Waldo at index " + ind + ".");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
@@ -18,4 +23,7 @@ Modify callback function.
 Get index of Waldo.
 
 Output: "Found Waldo at index 2."
+
+instead of for loop, use forEach
+
 */
